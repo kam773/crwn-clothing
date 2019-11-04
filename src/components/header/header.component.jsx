7e17c84982850ca3ecbc6f8ee92f52d89f39e0a2
kam773 <kamilsmas@gmail.com>
2019-11-04 13:22:44 +0100

    add redux and implement user signin

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { auth } from '../../firebase/firebase.utils';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
-import { auth } from '../../firebase/firebase.utils';
 
 const Header = ({ currentUser }) => (
   <div className='header'>
@@ -18,13 +20,21 @@ const Header = ({ currentUser }) => (
       <Link className='option' to='/shop'>
         CONTACT
       </Link>
-      {
-        currentUser ?
-          <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div> :
-          <Link to="/signin">SIGN IN</Link>
-      }
+      {currentUser ? (
+        <div className='option' onClick={() => auth.signOut()}>
+          SIGN OUT
+        </div>
+      ) : (
+          <Link className='option' to='/signin'>
+            SIGN IN
+        </Link>
+        )}
     </div>
   </div>
 );
 
-export default Header;
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
