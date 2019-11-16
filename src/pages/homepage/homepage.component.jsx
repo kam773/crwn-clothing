15c7cb2b6ea6bb159ca3dbd27247a7bc07cc8fff
kam773 <kamilsmas@gmail.com>
2019-11-16 15:56:44 +0100

    add perf optimization

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -5,7 +5,6 @@ import Directory from '../../components/directory/directory.component';
 import { HomePageContainer } from './homepage.styles'
 
 const HomePage = () => {
-  throw Error
   return (
     < HomePageContainer >
       <Directory />
