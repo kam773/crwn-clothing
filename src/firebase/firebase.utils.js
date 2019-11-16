9fb9fe73704cd62d773148a08c10d48fd2a86844
kam773 <kamilsmas@gmail.com>
2019-11-16 16:45:27 +0100

    cleanup

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,9 +2,6 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
 
-const apiKey = process.env.REACT_APP_API_KEY
-console.log(apiKey)
-
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
